0f4420a6858ef752f62e5f19a52ff64e144f7a1e
Talha Anwar <talhaanwer300@gmail.com>
2024-02-07 16:46:57 +0500

    cc

--- controllers/ShopifyController.js
+++ controllers/ShopifyController.js
@@ -11,84 +11,67 @@ const ShopifyAuthController = async (req, res, next) => {
     "/shopify/callback",
     false
   );
-  console.log("authRoute", authRoute, shop);
   res.redirect(authRoute);
 };
 
 const ShopifyCallbackController = async (req, res) => {
-  console.log("ShopifyCallbackController", req.query);
-  console.log("Incoming cookies:", req.cookies);
+  const client_session = await Shopify.Auth.validateAuthCallback(
+    req,
+    res,
+    req.query
+  );
+  const shop = req.query.shop;
 
-  try {
-    const client_session = await Shopify.Auth.validateAuthCallback(
-      req,
-      res,
-      req.query
-    );
-    const shop = req.query.shop;
+  // Use the Shopify API to get the shop details
+  const client = new Shopify.Clients.Rest(shop, client_session.accessToken);
+  const shopData = await client.get({
+    path: "shop",
+  });
+  // find if shop exists in db then update token else create new shop
 
-    // Use the Shopify API to get the shop details
-    const client = new Shopify.Clients.Rest(shop, client_session.accessToken);
-    const shopData = await client.get({
-      path: "shop",
-    });
-    // find if shop exists in db then update token else create new shop
+  const shopObj = {
+    name: shopData?.body?.shop?.name,
+    shop_url: shopData?.body?.shop?.domain,
+    shopify_token: client_session.accessToken,
+  };
 
-    const shopObj = {
-      name: shopData?.body?.shop?.name,
+  //   const shopExists = getShopByShopUrl(
+  //     "shop.json",
+  //     shopData?.body?.shop?.domain
+  //   );
+
+  const shopExists = await Shop.findOne({
+    where: {
       shop_url: shopData?.body?.shop?.domain,
-      shopify_token: client_session.accessToken,
-    };
-
-    //   const shopExists = getShopByShopUrl(
-    //     "shop.json",
-    //     shopData?.body?.shop?.domain
-    //   );
-
-    const shopExists = await Shop.findOne({
-      where: {
-        shop_url: shopData?.body?.shop?.domain,
+    },
+  });
+  if (shopExists) {
+    // updateShopByUrl("shop.json", shopData?.body?.shop?.domain, shopObj);
+    await Shop.update(
+      {
+        shopify_token: client_session.accessToken,
       },
-    });
-    if (shopExists) {
-      // updateShopByUrl("shop.json", shopData?.body?.shop?.domain, shopObj);
-      await Shop.update(
-        {
-          shopify_token: client_session.accessToken,
+      {
+        where: {
+          shop_url: shop,
         },
-        {
-          where: {
-            shop_url: shop,
-          },
-        }
-      );
-    } else {
-      // saveObjectsToFile("shop.json", shopObj);
-      const newShop = await Shop.create(shopObj);
-    }
-
-    const resp = await Shopify.Webhooks.Registry.register({
-      shop,
-      accessToken: client_session.accessToken,
-      path: "/shopify/webhooks/app/uninstalled",
-      topic: "APP_UNINSTALLED",
-      webhookHandler: async (topic, shop, body) => {
-        console.log(`Received webhook: ${topic} for shop: ${shop}`);
-      },
-    });
-    res.redirect("/shopify/success");
-  } catch (error) {
-    const { shop } = req.query;
-    const authRoute = await Shopify.Auth.beginAuth(
-      req,
-      res,
-      shop,
-      "/shopify/callback",
-      false
+      }
     );
-    console.log("authRoute", authRoute, shop);
-    res.redirect(authRoute);
+  } else {
+    // saveObjectsToFile("shop.json", shopObj);
+    const newShop = await Shop.create(shopObj);
   }
+
+  const resp = await Shopify.Webhooks.Registry.register({
+    shop,
+    accessToken: client_session.accessToken,
+    path: "/shopify/webhooks/app/uninstalled",
+    topic: "APP_UNINSTALLED",
+    webhookHandler: async (topic, shop, body) => {
+      console.log(`Received webhook: ${topic} for shop: ${shop}`);
+    },
+  });
+  res.redirect("/shopify/success");
 };
 
 const ShopifySucessController = async (req, res) => {
